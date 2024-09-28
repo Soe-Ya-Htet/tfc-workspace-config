ec87ca9594172471dc9a4339e3ee837fef172078
Henry <shakusoe@gmail.com>
2024-09-28 07:23:36 +0000

    Push updated

--- data.tf
+++ data.tf
@@ -1,3 +1,8 @@
 data "tfe_organization" "henry-cloud" {
   name = "henry-cloud"
 }
+
+data "tfe_project" "default-project" {
+  name = "Default Project"
+  organization = data.tfe_organization.henry-cloud.name
+}
--- main.tf
+++ main.tf
@@ -2,4 +2,6 @@ resource "tfe_workspace" "test" {
   name         = "hcp-vault-clust-ws"
   organization = data.tfe_organization.henry-cloud.name
   tag_names    = ["hcp-vault", "app"]
+  project_id = data.tfe_project.default-project.id
 }
+
